650b268167964768f4e65c008d51c430e70d3eb7
Chris LaRose <cjlarose@gmail.com>
2012-10-14 17:51:37 -0700

    Added structure from Rick's UML diagram

---
+++ src/view/DrawingMouseView.java
@@ -0,0 +1,14 @@
+package view;
+
+import java.util.Observable;
+import java.util.Observer;
+
+public class DrawingMouseView implements Observer {
+
+	@Override
+	public void update(Observable o, Object arg) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
