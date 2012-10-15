51fb8e3d27378ad69437c9f9230aaef87b59c626
Chris LaRose <cjlarose@gmail.com>
2012-10-15 16:49:03 -0700

    Formatted run tournament

--- src/model/RunTournament.java
+++ src/model/RunTournament.java
@@ -5,17 +5,26 @@ public class RunTournament {
 	public static void main(String[] args) {
 		Player beginner_player = new AIPlayer(new BeginnerStrategy());
 		Player intermediate_player = new AIPlayer(new IntermediateStrategy());
+		
+		System.out.println("Result of playing 1000 games when beginner goes first:");
+		RunTournament.playTournament(beginner_player, intermediate_player, 1000);
+		
+		System.out.println();
+		
+		System.out.println("Result of playing 1000 games when intermediate goes first:");
+		RunTournament.playTournament(intermediate_player, beginner_player, 1000);
+	}
+	
+	private static void playTournament(Player player1, Player player2, int numGames) {
+		Tournament t = new Tournament(player1, player2);
 
-		Tournament t = new Tournament(beginner_player, intermediate_player);
-		for (int i = 0; i < 1000; i++)
+		for (int i = 0; i < numGames; i++)
 			t.playGame();
 
-		int p1_wins = t.getWins(beginner_player);
-		int p2_wins = t.getWins(intermediate_player);
+		int p1_wins = t.getWins(player2);
+		int p2_wins = t.getWins(player2);
 		int ties = t.getWins(null);
 
-		System.out
-				.println("Result of playing 1000 games when beginner goes first:");
 		System.out.println(String.format(
 				"Beginner: %s\nTies: %s\nIntermediate: %s", p1_wins, p2_wins,
 				ties));
