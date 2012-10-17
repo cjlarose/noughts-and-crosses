e7c78198f622c6e1465ed30f08cc1ac5d03cdbfc
Chris LaRose <cjlarose@gmail.com>
2012-10-17 16:59:40 -0700

    Removed list of games in controller

--- src/controller/Controller.java
+++ src/controller/Controller.java
@@ -2,8 +2,6 @@ package controller;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.util.LinkedList;
-import java.util.List;
 
 import view.*;
 import model.*;
@@ -13,13 +11,13 @@ import model.*;
  * In other words, the controller is responsible for the business logic of the
  * view. For instance, the view is responsible for reporting the coordinates
  * of the cell a user chooses, but the Controller is responsible for passing
- * that move onto the Game
+ * that move onto the Game. The view, in essence, should never directly send 
+ * messages to the Game.
  */
 public class Controller {
 
-	private List<Game> games;
 	private MainGUI view;
-	private Game current_game;
+	private Game game;
 	
 	public static void main(String[] args) {
 		// this will be the _only_ main method of the project
@@ -28,7 +26,7 @@ public class Controller {
 	
 	public Controller() {
 		this.view.addStrategyChangedListener(new strategyChangedListener());
-		this.games = new LinkedList<Game>();
+		//this.game = new Game();
 	}
 	
 	public class strategyChangedListener implements ActionListener {
