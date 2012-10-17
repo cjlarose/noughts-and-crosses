40d391c1938fd87006661e5d5d65c6a08dc7607f
Tanner Prynn <tannerprynn@gmail.com>
2012-10-16 17:00:44 -0700

    testing pushing

--- src/model/BeginnerStrategy.java
+++ src/model/BeginnerStrategy.java
@@ -5,6 +5,7 @@ public class BeginnerStrategy implements Strategy {
 	@Override
 	/**
 	 * The beginner strategy is just to choose the next unoccupied space from the top left.
+	 * @return a 2-int array [i,j] with the location of the move
 	 */
 	public int[] getMove(Game g, Player p) {
 		for (int i = 0; i < 9; i++) {
