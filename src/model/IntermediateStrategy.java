54af91bd2a7cbac64d52a553781544eb381ca124
Reid Knight <knightr@email.arizona.edu>
2012-10-17 13:29:57 -0700

    Added comments to Intermediate

--- src/model/IntermediateStrategy.java
+++ src/model/IntermediateStrategy.java
@@ -9,7 +9,11 @@ public class IntermediateStrategy implements Strategy {
 			24, 144, 130, 18, 6, 5, 3, 72, 65, 9, 272, 257, 17, 80, 68, 20 };
 	private final int[] wins = { 448, 56, 7, 292, 146, 73, 273, 84 };
 	private final LinkedList<Integer> single_moves;
-
+	
+	/**
+	 * Constructs the list used to determine if a almost win to block
+	 * or win is a valid, single move. 
+	 */
 	public IntermediateStrategy() {
 		single_moves = new LinkedList<Integer>();
 		for (int i = 0; i < 9; i++) {
@@ -17,11 +21,17 @@ public class IntermediateStrategy implements Strategy {
 		}
 	}
 
+	/**
+	 * @param g is the game to which the move will be added
+	 * @ param p The current player. The AIPlayer, in the case of this class.
+	 */
 	@Override
 	public int[] getMove(Game g, Player p) {
 		int computer_moves = 0;
 		int player_moves = 0;
 		int temp_one = 1;
+		//Building the lists of computer and player moves, as the original method
+		//cannot differentiate
 		Player[][] board = g.toMatrix();
 		for (int i = 0; i < 3; i++) {
 			for (int j = 0; j < 3; j++) {
@@ -34,7 +44,9 @@ public class IntermediateStrategy implements Strategy {
 				}
 			}
 		}
-		// gar. This be a pain in the ass.
+		//Checks for an almost win situation, and responds accordingly.
+		//First: if the almost win situation is in the favor of the computer,
+		//this will return a move that will win the game. 
 		for (int i : almost_wins) {
 			if ((computer_moves & i) == i) {
 				for (int j : wins) {
@@ -52,7 +64,9 @@ public class IntermediateStrategy implements Strategy {
 					}
 				}
 			}
-
+			//Second: if the almost win situation is in favor of the other player,
+			//this will return a move that will block the win (blocks only the first occurrence of
+			//one)
 			if ((player_moves & i) == i) {
 				for (int j : wins) {
 					int loc = j ^ i;
@@ -70,6 +84,7 @@ public class IntermediateStrategy implements Strategy {
 				}
 			}
 		}
+		//Finally: if there is no almost win situation, returns a random valid move. 
 		LinkedList<Integer> remaining_moves = g.getRemainingMoves();
 		Random rand = new Random();
 
