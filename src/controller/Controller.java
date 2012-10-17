f3a2c3b67d767d2eef5e6f70c574a1437c8ed0c8
Chris LaRose <cjlarose@gmail.com>
2012-10-17 16:54:34 -0700

    Added controller and main gui classes

---
+++ src/controller/Controller.java
@@ -0,0 +1,43 @@
+package controller;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.LinkedList;
+import java.util.List;
+
+import view.*;
+import model.*;
+
+/**
+ * This class is responsible for the coordination between views and the model.
+ * In other words, the controller is responsible for the business logic of the
+ * view. For instance, the view is responsible for reporting the coordinates
+ * of the cell a user chooses, but the Controller is responsible for passing
+ * that move onto the Game
+ */
+public class Controller {
+
+	private List<Game> games;
+	private MainGUI view;
+	private Game current_game;
+	
+	public static void main(String[] args) {
+		// this will be the _only_ main method of the project
+		new Controller();
+	}
+	
+	public Controller() {
+		this.view.addStrategyChangedListener(new strategyChangedListener());
+		this.games = new LinkedList<Game>();
+	}
+	
+	public class strategyChangedListener implements ActionListener {
+
+		@Override
+		public void actionPerformed(ActionEvent evt) {
+			Strategy s = view.getSelectedStrategy();
+			// set the ai's player's strategy to s
+		}
+		
+	}
+}
---
+++ src/view/MainGUI.java
@@ -0,0 +1,20 @@
+package view;
+
+import java.awt.event.ActionListener;
+
+import javax.swing.JFrame;
+
+import model.Strategy;
+
+public class MainGUI extends JFrame {
+
+	public void addStrategyChangedListener(ActionListener l) {
+		// TODO Add the listener to the ui element that handles changing the strategy
+	}
+
+	public Strategy getSelectedStrategy() {
+		// TODO Return the currently selected strategy
+		return null;
+	}
+
+}
