969980ab936eb6523837deebae4a452200620502
Chris LaRose <cjlarose@gmail.com>
2012-10-16 18:51:11 -0700

    Basic run tournament complete

--- src/model/RunTournament.java
+++ src/model/RunTournament.java
@@ -1,33 +1,56 @@
 package model;
 
-public class RunTournament {
+import java.util.Observable;
+import java.util.Observer;
+import java.util.Scanner;
+
+public class RunTournament implements Observer {
+	
+	int p1_wins = 0;
+	int p2_wins = 0;
+	int ties = 0;
+	Player beginner_player;
+	Player intermediate_player;
 
 	public static void main(String[] args) {
-		Player beginner_player = new AIPlayer(new BeginnerStrategy());
-		Player intermediate_player = new AIPlayer(new IntermediateStrategy());
+		RunTournament rt = new RunTournament();
+		
+		rt.beginner_player = new AIPlayer(new BeginnerStrategy());
+		rt.intermediate_player = new AIPlayer(new IntermediateStrategy());
+		
 		
 		System.out.println("Result of playing 1000 games when beginner goes first:");
-		RunTournament.playTournament(beginner_player, intermediate_player, 1000);
+		rt.playTournament(rt.beginner_player, rt.intermediate_player, 1000);
 		
 		System.out.println();
 		
 		System.out.println("Result of playing 1000 games when intermediate goes first:");
-		RunTournament.playTournament(intermediate_player, beginner_player, 1000);
+		rt.playTournament(rt.intermediate_player, rt.beginner_player, 1000);
 	}
 	
-	private static void playTournament(Player player1, Player player2, int numGames) {
-		Tournament t = new Tournament(player1, player2);
+	private void playTournament(Player player1, Player player2, int numGames) {
 
-		for (int i = 0; i < numGames; i++)
-			t.playGame();
-
-		int p1_wins = t.getWins(player2);
-		int p2_wins = t.getWins(player2);
-		int ties = t.getTies();
+		for (int i = 0; i < numGames; i++) {
+			Game g = new Game(player1, player2);
+			g.addObserver(this);
+			this.update(g, null);
+		}
 
 		System.out.println(String.format(
 				"Beginner: %s\nTies: %s\nIntermediate: %s", p1_wins, p2_wins,
 				ties));
+	}
+
+	@Override
+	public void update(Observable game, Object arg) {
+		if (((Game) game).isFinished()) {
+			System.out.println("finished game");
+		} else {
+			Player current_player = ((Game) game).getCurrentPlayer();
+			System.out.println(String.format("%s's turn", current_player == beginner_player ? "Beginner" : "Intermediate"));
+			int[] move = ((AIPlayer) current_player).getMove((Game) game);
+			((Game) game).makeMove(current_player, move[0], move[1]);
+		}
 	}
 
 }
