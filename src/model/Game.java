4dafe654454bd8a068901074a154e933496b8e30
Chris LaRose <cjlarose@gmail.com>
2012-10-16 19:23:12 -0700

    Refactored variable names in Game::toMatrix()

--- src/model/Game.java
+++ src/model/Game.java
@@ -100,13 +100,13 @@ public class Game extends Observable {
 		int tmp2 = this.player2_moves;
 		
 		Player[][] matrix = new Player[3][3];
-		for (int i = 0; i < 9; i++) {
-			int y = i / 3;
-			int x = i % 3;
+		for (int n = 0; n < 9; n++) {
+			int i = n / 3;
+			int j = n % 3;
 			if ((tmp1 & 1) == 1)
-				matrix[y][x] = this.player1;
+				matrix[i][j] = this.player1;
 			else if ((tmp2 & 1) == 1)
-				matrix[y][x] = this.player2;
+				matrix[i][j] = this.player2;
 			tmp1 >>= 1;
 			tmp2 >>= 1;
 		}
