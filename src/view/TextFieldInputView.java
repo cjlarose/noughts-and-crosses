678c90b327a6c76a6506436124a54cad068ffc18
Chris LaRose <cjlarose@gmail.com>
2012-10-22 19:39:47 -0700

    Put column and row inputs in flow layout

--- src/view/TextFieldInputView.java
+++ src/view/TextFieldInputView.java
@@ -1,8 +1,9 @@
 package view;
 
 import java.awt.BorderLayout;
+import java.awt.Dimension;
+import java.awt.FlowLayout;
 import java.awt.Font;
-import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.util.Observable;
@@ -21,13 +22,8 @@ import controller.Controller;
 public class TextFieldInputView extends JPanel implements Observer {
 	private JPanel game;
 	private JTextArea game_view;
-	private JPanel user_input;
-	private JPanel buttons;
-	private JLabel row;
-	private JTextField rowInput;
-	private JLabel col;
-	private JTextField colInput;
 	private JButton make_move;
+	private JPanel user_input;
 	private Controller c;
 
 	public TextFieldInputView() {
@@ -52,48 +48,62 @@ public class TextFieldInputView extends JPanel implements Observer {
 		
 		game_view.setFont(monospace);
 		game.add(game_view, "Center");
-
-		// controls container
-		user_input = new JPanel();
-		user_input.setVisible(true);
-		user_input.setLayout(new GridLayout(2, 2));
 		
-		// row
-		row = new JLabel("Row:");
-		rowInput = new JTextField("");
-		user_input.add(row);
-		user_input.add(rowInput);
-		
-		// column
-		col = new JLabel("Column:");
-		colInput = new JTextField("");
-		user_input.add(col);
-		user_input.add(colInput);
-		
-		// Make move button
-		buttons = new JPanel();
-		buttons.setVisible(true);
-		make_move = new JButton("Make your move");
-		make_move.addActionListener(new MoveButtonListener());
-		buttons.setLayout(new BorderLayout(10, 10));
-		buttons.add(make_move, "Center");
+		this.user_input = new ControlsContainer();
 		
 		add(game, "North");
 		add(user_input, "Center");
-		add(buttons, "South");
 		
 	}
 	
-	public class MoveButtonListener implements ActionListener {
-
-		@Override
-		public void actionPerformed(ActionEvent arg0) {
-			System.out.println("Clicked");
-			int i = Integer.parseInt(colInput.getText());
-			int j = Integer.parseInt(rowInput.getText());
-			c.makeMove(i, j);
+	public class ControlsContainer extends JPanel {
+		
+		private JPanel buttons;
+		private JLabel row;
+		private JTextField rowInput;
+		private JLabel col;
+		private JTextField colInput;
+		
+		public ControlsContainer() {
+			// controls container
+			this.setVisible(true);
+			this.setLayout(new FlowLayout());
+			
+			// row
+			row = new JLabel("Row:");
+			rowInput = new JTextField();
+			rowInput.setPreferredSize(new Dimension(30, 20));
+			this.add(row);
+			this.add(rowInput);
+			
+			// column
+			col = new JLabel("Column:");
+			colInput = new JTextField();
+			colInput.setPreferredSize(new Dimension(30, 20));
+			this.add(col);
+			this.add(colInput);
+			
+			// Make move button
+			buttons = new JPanel();
+			buttons.setVisible(true);
+			make_move = new JButton("Make your move");
+			make_move.addActionListener(new MoveButtonListener());
+			buttons.setLayout(new BorderLayout(10, 10));
+			buttons.add(make_move, "Center");
+			this.add(buttons);
 		}
 		
+		public class MoveButtonListener implements ActionListener {
+	
+			@Override
+			public void actionPerformed(ActionEvent arg0) {
+				System.out.println("Clicked");
+				int i = Integer.parseInt(colInput.getText());
+				int j = Integer.parseInt(rowInput.getText());
+				c.makeMove(i, j);
+			}
+			
+		}
 	}
 
 	//
