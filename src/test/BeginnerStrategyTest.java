76469751004f10b3d315c8ca8d1d7ec4d3df2a84
Tanner Prynn <tannerprynn@gmail.com>
2012-10-24 17:58:19 -0700

    testing of beginner strategy

---
+++ src/test/BeginnerStrategyTest.java
@@ -0,0 +1,34 @@
+package test;
+
+import static org.junit.Assert.*;
+import model.BeginnerStrategy;
+import model.Game;
+
+import org.junit.Test;
+
+public class BeginnerStrategyTest {
+
+	@Test
+	public void testGetMove() {
+		Game g = new Game();
+		BeginnerStrategy b = new BeginnerStrategy();
+		int[] move = b.getMove(g, 'X');
+		assertTrue(move[0] >= 0 && move[0] <= 2);
+		assertTrue(move[1] >= 0 && move[1] <= 2);
+		
+		//Move in every space except 2,2
+		g.makeMove(0, 0);
+		g.makeMove(0, 1);
+		g.makeMove(0, 2);
+		g.makeMove(1, 1);
+		g.makeMove(1, 0);
+		g.makeMove(1, 2);
+		g.makeMove(2, 1);
+		g.makeMove(2, 0);
+
+		//This should get coverage of the while loop with probability 8/9
+		move = b.getMove(g, 'O');
+		assertTrue(move[0] == 2 && move[1] == 2);
+	}
+
+}
