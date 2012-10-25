39faf45063ce30935decdba41daa5cc5a917bb3c
Tanner Prynn <tannerprynn@gmail.com>
2012-10-24 18:27:19 -0700

    update Player

---
+++ src/test/RunTournamentTest.java
@@ -0,0 +1,14 @@
+package test;
+
+import org.junit.Test;
+
+import model.RunTournament;
+
+public class RunTournamentTest {
+
+	@Test
+	public void testRunTournament() {
+		RunTournament r = new RunTournament();
+	}
+
+}
